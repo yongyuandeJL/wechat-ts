9135961a13f782c0478539bb4238e9722d2f162b
yongyuandeJL <52713147+yongyuandeJL@users.noreply.github.com>
2022-10-12 11:03:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU16833T0La7skhllxTEwMSqYt3vgyFF5JJ6GFfi',
+      id: 'PDU16833T0La7skhIIxTEwMSqYt3vgyFF5JJ6GFfi',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
