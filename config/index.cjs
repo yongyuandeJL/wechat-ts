f05b50512f23a04c6bf0d847f308c8257cdd51db
yongyuandeJL <52713147+yongyuandeJL@users.noreply.github.com>
2022-10-12 11:09:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,38 +13,41 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '',
+  CITY: '',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小韩',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU16833T0La7skhIIxTEwMSqYt3vgyFF5JJ6GFfi',
+      id: 'PDU16832T7l056huNZVUlqbO9TNqwqq9MLs6lYlh8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
+      // 所在省份
+      province: "四川",
+      // 所在城市
+      city: "成都",
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '04-16',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://www.zwcsm.com/",
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '自己', year: '1998', date: '04-16',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日', name: '小吴', year: '1995', date: '05-26',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2021-01-20' },
       ],
     },
   ],
